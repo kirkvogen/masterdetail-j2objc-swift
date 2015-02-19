af988cb8305316bffd98d1be05ad56985149dacb
Kirk Vogen <kirk@vogen.org>
2015-02-18 21:35:22 -0600

    Made mass copyright statement update. Also, made minor edit to instructions in README file.

--- masterdetail-lib/src/main/java/masterdetail/model/DetailEntry.java
+++ masterdetail-lib/src/main/java/masterdetail/model/DetailEntry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Kirk C. Vogen
+ * Copyright 2014-2015 Kirk C. Vogen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
